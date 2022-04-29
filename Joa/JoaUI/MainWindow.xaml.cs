6cf2de148e2738ceace1d2a1d9e290350fd0f6a8
Gian-LucaZ <gianluca.zwahlen@cmiag.ch>
2022-04-29 14:18:11 +0200

    feat(UI): UI hinzugefuegt [14:18:30]

---
+++ Joa/JoaUI/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using PluginBase;
+
+namespace JoaUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateList(object sender, ISearchResult result)
+        {
+            ResultList.Items.Add(new SearchItem(result));
+        }
+        
+        private void TextBoxBase_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+    }
+}
