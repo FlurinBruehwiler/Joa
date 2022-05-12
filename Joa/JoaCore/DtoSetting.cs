5e02337e3967a6dcb83bda0b718370d16998254e
Flurin Brühwiler <47397416+FlurinBruehwiler@users.noreply.github.com>
2022-05-12 21:47:51 +0200

    feat(Settings): Settings Deserializaiton WIP [12.05.2022]

--- Joa/JoaCore/DtoSetting.cs
+++ Joa/JoaCore/DtoSetting.cs
@@ -1,13 +1,99 @@
-﻿namespace JoaCore;
+﻿using System.Text.Json;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace JoaCore;
 
 public class DtoSetting
 {
-    public Dictionary<string, Dictionary<string, object>> PluginSettings { get; set; }
-    
+    private Dictionary<string, Dictionary<string, object>> _pluginSettings;
+
+    public Dictionary<string, Dictionary<string, object>> PluginSettings
+    {
+        get => _pluginSettings;
+        set
+        {
+            Dictionary<string, Dictionary<string, object>> newPluginSettings = new();
+
+            foreach (var (key, pluginSetting) in value)
+            {
+                newPluginSettings.Add(key, ConvertPlugin(pluginSetting));
+            }
+
+            _pluginSettings = newPluginSettings;
+        }
+    }
+
+    Dictionary<string, object> ConvertPlugin(Dictionary<string, object> pluginSettings)
+    {
+        Dictionary<string, object> newDictionary = new();
+            
+        foreach (var (key, settingValue) in pluginSettings)
+        {
+            if (settingValue is not JsonElement jsonElement)
+            {
+                newDictionary.Add(key, settingValue);
+                continue;
+            }
+                
+            var type = jsonElement.ValueKind switch
+            {
+                JsonValueKind.Number => typeof(int),
+                JsonValueKind.String => typeof(string),
+                JsonValueKind.True => typeof(bool),
+                JsonValueKind.False => typeof(bool),
+                JsonValueKind.Array => typeof(List<Dictionary<string, object>>),
+                _ => throw new JsonException()
+            };
+
+            var newValue = jsonElement.Deserialize(type) ?? throw new JsonException();
+
+            if (newValue is List<Dictionary<string, object>> list)
+            {
+                newValue = list.Select(ConvertListItem).ToList();
+            }
+                
+            newDictionary.Add(key, newValue);
+        }
+
+        return newDictionary;
+    }
+
+    Dictionary<string, object> ConvertListItem(Dictionary<string, object> settings)
+    {
+        Dictionary<string, object> newDictionary = new();
+            
+        foreach (var (key, settingValue) in settings)
+        {
+            if (settingValue is not JsonElement jsonElement)
+            {
+                newDictionary.Add(key, settingValue);
+                continue;
+            }
+                
+            var type = jsonElement.ValueKind switch
+            {
+                JsonValueKind.Number => typeof(int),
+                JsonValueKind.String => typeof(string),
+                JsonValueKind.True => typeof(bool),
+                JsonValueKind.False => typeof(bool),
+                _ => throw new JsonException()
+            };
+
+            newDictionary.Add(key, jsonElement.Deserialize(type) ?? throw new JsonException());
+        }
+
+        return newDictionary;
+    }
+
     public DtoSetting(Settings setting)
     {
         PluginSettings = setting.PluginDefinitions.ToDictionary(pluginDefinition => pluginDefinition.Name,
             pluginDefinition => pluginDefinition.PluginSettings.ToDictionary(pluginSetting => pluginSetting.Name,
                 pluginSetting => pluginSetting.Value));
+    }
+
+    public DtoSetting()
+    {
+        
     }
 }
--- Joa/TestRunner/Program.cs
+++ Joa/TestRunner/Program.cs
@@ -3,61 +3,22 @@ using System.Text.Json;
 using JoaCore;
 
 var search = new Search();
-var filename = Path.GetFullPath(Path.Combine(typeof(PluginLoader).Assembly.Location, @"..\..\..\..\..\settings.json"));
-var options = new JsonSerializerOptions { WriteIndented = true };
-
-var settings = new Test
+var filename = Path.GetFullPath(Path.Combine(typeof(PluginLoader).Assembly.Location,
+    @"..\..\..\..\..\settings.json"));
+var options = new JsonSerializerOptions
 {
-    Property = new Dictionary<string, object>
-    {
-        { "test1", 1 },
-        { "test2", "2" },
-        { "test3", true },
-        {
-            "test4", new Dictionary<string, Dictionary<string, object>>()
-        }
-    }
+    WriteIndented = true
 };
 
 
-var jsonString = JsonSerializer.Serialize(settings);
-File.WriteAllText(filename, jsonString);
-var result = JsonSerializer.Deserialize<Test>(jsonString);
+var pluginLoader = new PluginLoader();
+var coreSettings = new CoreSettings();
+var plugins = pluginLoader.InstantiatePlugins(coreSettings);
+var settings = new Settings(coreSettings,
+    plugins);
+var dtoSetting = new DtoSetting(settings);
+var jsonString = JsonSerializer.Serialize(dtoSetting, options);
+File.WriteAllText(filename,
+    jsonString);
+var result = JsonSerializer.Deserialize<DtoSetting>(jsonString);
 Console.Write("4124");
-
-internal class Test
-{
-    private Dictionary<string, object> _property;
-
-    public Dictionary<string, object> Property
-    {
-        get => _property;
-        set
-        {
-            _property = value;
-            Dictionary<string, object> newDictionary = new();
-            
-            foreach (var (key, settingValue) in value)
-            {
-                if (settingValue is not JsonElement jsonElement)
-                {
-                    newDictionary.Add(key, settingValue);
-                    continue;
-                }
-                
-                var type = jsonElement.ValueKind switch
-                {
-                    JsonValueKind.Number => typeof(int),
-                    JsonValueKind.String => typeof(string),
-                    JsonValueKind.True => typeof(bool),
-                    JsonValueKind.Array => typeof(List<>),
-                    _ => throw new JsonException()
-                };
-            
-                newDictionary.Add(key, jsonElement.Deserialize(type) ?? throw new JsonException());
-            }
-            
-            _property = newDictionary;
-        }
-    }
-}
