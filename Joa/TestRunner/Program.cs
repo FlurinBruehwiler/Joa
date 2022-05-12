cbb50999c1749d1d4cc8b68002f088cca5256791
FlurinBruehwilerCMI <69467225+FlurinBruehwilerCMI@users.noreply.github.com>
2022-05-12 17:18:05 +0200

    feat(Settings): Settings Deserializaion WIP [12.05.2022]

--- Joa/TestRunner/Program.cs
+++ Joa/TestRunner/Program.cs
@@ -1,9 +1,63 @@
-﻿using System.Text.Json;
+﻿using System.Reflection.Metadata;
+using System.Text.Json;
 using JoaCore;
 
 var search = new Search();
 var filename = Path.GetFullPath(Path.Combine(typeof(PluginLoader).Assembly.Location, @"..\..\..\..\..\settings.json"));
 var options = new JsonSerializerOptions { WriteIndented = true };
-var dtoSetting = new DtoSetting(search.Settings);
-var jsonString = JsonSerializer.Serialize(dtoSetting, options);
+
+var settings = new Test
+{
+    Property = new Dictionary<string, object>
+    {
+        { "test1", 1 },
+        { "test2", "2" },
+        { "test3", true },
+        {
+            "test4", new Dictionary<string, Dictionary<string, object>>()
+        }
+    }
+};
+
+
+var jsonString = JsonSerializer.Serialize(settings);
 File.WriteAllText(filename, jsonString);
+var result = JsonSerializer.Deserialize<Test>(jsonString);
+Console.Write("4124");
+
+internal class Test
+{
+    private Dictionary<string, object> _property;
+
+    public Dictionary<string, object> Property
+    {
+        get => _property;
+        set
+        {
+            _property = value;
+            Dictionary<string, object> newDictionary = new();
+            
+            foreach (var (key, settingValue) in value)
+            {
+                if (settingValue is not JsonElement jsonElement)
+                {
+                    newDictionary.Add(key, settingValue);
+                    continue;
+                }
+                
+                var type = jsonElement.ValueKind switch
+                {
+                    JsonValueKind.Number => typeof(int),
+                    JsonValueKind.String => typeof(string),
+                    JsonValueKind.True => typeof(bool),
+                    JsonValueKind.Array => typeof(List<>),
+                    _ => throw new JsonException()
+                };
+            
+                newDictionary.Add(key, jsonElement.Deserialize(type) ?? throw new JsonException());
+            }
+            
+            _property = newDictionary;
+        }
+    }
+}
