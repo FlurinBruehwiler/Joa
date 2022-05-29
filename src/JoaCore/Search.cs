04f5456a7292f92baa7a2b795750fd429c2947d3
Flurin Brühwiler <47397416+FlurinBruehwiler@users.noreply.github.com>
2022-05-29 20:53:50 +0200

    feat(Search): Only matching plugins are being queried [29.05.2022]

--- src/JoaCore/Search.cs
+++ src/JoaCore/Search.cs
@@ -1,5 +1,6 @@
 ﻿using Interfaces;
 using Interfaces.Logger;
+using JoaCore.PluginCore;
 using JoaCore.Settings;
 using Microsoft.Extensions.Configuration;
 
@@ -23,24 +24,36 @@ public class Search
 
     public async Task ExecuteSearchResult(Guid pluginId, ISearchResult searchResult)
     {
-        var pluginDef = PluginManager.Plugins.First(p => p.Id == pluginId);
+        var pluginDef = PluginManager.Plugins?.First(p => p.Id == pluginId);
+        if (pluginDef is null)
+            return;
         await Task.Run(() => pluginDef.Plugin.Execute(searchResult)); //ToDo Check if it is really async
     }
 
     public async Task UpdateSearchResults(string searchString)
     {
+        if (PluginManager.Plugins == null)
+            return;
+        
         var timer = JoaLogger.GetInstance().StartMeasure();
         
         SearchResults.Clear();
+
+        var matchingPlugins = await GetMatchingPlugins(searchString);
+        
+        if (matchingPlugins.Count != 0)
+        {
+            matchingPlugins = PluginManager.Plugins.Where(x => x.StrictPlugin is not null).ToList();
+        }
+        
         var pluginsTasks = new Dictionary<Task<List<ISearchResult>>, Guid>();
 
-        foreach (var pluginDef in PluginManager.Plugins)
+        foreach (var pluginDef in matchingPlugins)
         {
-            var plugin = pluginDef.Plugin;
-            var pluginTask = Task.Run(() => plugin.GetResults(searchString));
+            var pluginTask = Task.Run(() => pluginDef.Plugin.GetResults(searchString));
             pluginsTasks.Add(pluginTask, pluginDef.Id);
         }
-
+        
         while (pluginsTasks.Count > 0)
         {
             var pluginTask = await Task.WhenAny(pluginsTasks.Keys);
@@ -64,5 +77,31 @@ public class Search
         }
         
         JoaLogger.GetInstance().LogMeasureResult(timer,$"{nameof(UpdateSearchResults)}:{searchString}");
+    }
+
+    private async Task<List<PluginDefinition>> GetMatchingPlugins(string searchString)
+    {
+        if (PluginManager.Plugins == null)
+            return new List<PluginDefinition>();
+
+        List<Task<(bool, Guid)>> validatorTasks = new();
+        
+        foreach (var pluginDef in PluginManager.Plugins)
+        {
+            if(pluginDef.StrictPlugin is null)
+                continue;
+
+            validatorTasks.Add(Task.Run(() => ValidatorWrapper(pluginDef, searchString)));
+        }
+
+        var results = await Task.WhenAll(validatorTasks);
+
+        return results.Select(result =>
+            PluginManager.Plugins.First(plugin => plugin.Id == result.Item2)).ToList();
+    }
+
+    private async Task<(bool, Guid)> ValidatorWrapper(PluginDefinition pluginDef, string searchString)
+    {
+        return (await Task.Run(() => pluginDef.StrictPlugin!.Validator(searchString)), pluginDef.Id);
     }
 }
