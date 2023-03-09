986cf72bbda28dc91caf744202be03979aae3c10
Flurin Brühwiler <bruhwiler.flurin@gmail.com>
2023-03-09 23:00:23 +0100

    feat(Icons): Icons of shortcuts now sometimes do not have the arrow, very much WIP [09.03.2023]

--- src/Joa/Injectables/IconHelper.cs
+++ src/Joa/Injectables/IconHelper.cs
@@ -3,18 +3,15 @@ using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 using JoaLauncher.Api.Injectables;
 
+// using Shell32;
+
 namespace Joa.Injectables;
 
 public class IconHelper : IIconHelper
 {
     public string GetIconsDirectory(Type pluginType)
     {
-        var pluginDirectory = Path.GetDirectoryName(pluginType.Assembly.Location);
-
-        if (pluginDirectory is null)
-            throw new Exception(); //ToDo
-
-        var iconsLocation = Path.Combine(pluginDirectory, "Icons");
+        var iconsLocation = Path.Combine("wwwroot", "Icons");
 
         if (!Directory.Exists(iconsLocation))
             Directory.CreateDirectory(iconsLocation);
@@ -24,10 +21,17 @@ public class IconHelper : IIconHelper
 
     public string CreateIconFromFileIfNotExists<T>(string fileLocation)
     {
-        if (!File.Exists(fileLocation))
-            throw new Exception($"Could not find the following file: {fileLocation}");
+        // throw new Exception($"Could not find the following file: {fileLocation}");
+        
+        if (fileLocation.EndsWith(".lnk"))
+            fileLocation = GetShortcutTargetFile(fileLocation);
 
-        var iconLocation = Path.Combine(GetIconsDirectory(typeof(T)), Path.ChangeExtension(Path.GetFileName(fileLocation), ".png"));
+        if (!File.Exists(fileLocation))
+            return string.Empty;
+        
+        var iconLocation = Path
+            .Combine(GetIconsDirectory(typeof(T)), Path.ChangeExtension(Path.GetFileName(fileLocation), ".png"))
+            .Replace(" ", "%20");
 
         if (File.Exists(iconLocation))
             return iconLocation;
@@ -44,6 +48,31 @@ public class IconHelper : IIconHelper
 
         bitmapIcon.Save(iconLocation, ImageFormat.Png);
 
-        return iconLocation;
+        return Path.GetFileName(iconLocation);
+    }
+
+    private string GetShortcutTargetFile(string file)
+    {
+        try
+        {
+            var pathOnly = Path.GetDirectoryName(file);
+            var filenameOnly = Path.GetFileName(file);
+
+            dynamic shell = Activator.CreateInstance(Type.GetTypeFromProgID("shell.application", true));
+            var folder = shell.NameSpace(pathOnly);
+            var folderItem = folder.ParseName(filenameOnly);
+            if (folderItem != null)
+            {
+                return folderItem.GetLink.Path;
+            }
+
+            return string.Empty;
+        }
+        catch (Exception e)
+        {
+            JoaLogger.GetInstance().Error(e.Message);
+        }
+
+        return string.Empty; 
     }
 }
